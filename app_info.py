625cf3f5fc42ada6850f6bf1c4ee1a845464349b
XandeYong <yongzuoxuan@gmail.com>
2025-10-30 09:04:56 +0800

    Included test data files for test cases

--- app_info.py
+++ app_info.py
@@ -44,7 +44,7 @@ ENVIRONMENT = {
 - rename main.py to mediaoptimizer.py
 
 1.3.0.0
-- Publish media to Google Photos
+- Added feature, Publish media to Google Photos
 - Added 1 argument (operation) to allow user to choose operation between (0: all, 1: optimize only, 2: upload only)
 - Added 2 folders to manage uploaded and failed upload media
 - Removed failed_log file
@@ -54,6 +54,7 @@ ENVIRONMENT = {
 - Fixed video optimization interruption issue (subprocess not handled properly)
 - Created 2 test cases (test_extension.py, test_magic.py) and added test data files
 - Added pyproject.toml for project management
+- Added test data files for test cases
 
 [Future Release]
 x.x.0.0
